d24dbe381e5051b8a4b8833d567b97b3315ab2d7
Rick Tesmond <tesmonrd@gmail.com>
2016-03-07 19:04:56 -0800

    working message to server using command line

---
+++ src/client.py
@@ -0,0 +1,21 @@
+# _*_utf8_*_
+
+import socket
+
+
+def client(message):
+    infos = socket.getaddrinfo('127.0.0.1', 5000)
+    stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
+    client = socket.socket(*stream_info[:3])
+    client.connect(stream_info[-1])
+    client.sendall(message.encode('utf8'))
+
+    buffer_length = 8
+    reply_complete = False
+    while not reply_complete:
+        part = client.recv(buffer_length)
+        print(part.decode('utf8'))
+        if len(part) < buffer_length:
+            break
+
+client(input(u"What is your message:"))
