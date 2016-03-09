69b0fa49ddd39dd5d15531e5470913b059f07e5a
Rick Tesmond <tesmonrd@gmail.com>
2016-03-08 18:22:52 -0800

    working server with http protocols returning to server, also server remains open

--- src/server.py
+++ src/server.py
@@ -10,14 +10,43 @@ def server():
     server.bind(address)
     server.listen(1)
     conn, addr = server.accept()
-    buffer_length = 8
-    message_complete = False
-    while not message_complete:
-        part = conn.recv(buffer_length)
-        print(part.decode('utf8'))
-        conn.sendall(part)
-        if len(part) < buffer_length:
-            break
+
+    try:
+        while True:
+            incoming_message = ''
+            response = response_ok()
+            buffer_length = 25
+            message_complete = False
+            while not message_complete:
+                part = conn.recv(buffer_length)
+                decoded = part.decode('utf8')
+                incoming_message += decoded
+                if len(part) < buffer_length:
+                    break
+            print(incoming_message)
+            conn.sendall(response)
+            conn.close()
+            server.listen(1)
+            conn, addr = server.accept()
+    except KeyboardInterrupt:
+        print('KeyboardInterrupted')
+        server.close()
+    except:
+        response = response_error()
+        conn.sendall(response)
+        conn.close()
+        server.listen(1)
+        conn, addr = server.accept()
+
+
+def response_ok():
+    reply_ok = 'HTTP/1.1 200 OK\nContent-Type: text/plain\n\r\nYou Made It!'
+    return reply_ok.encode('utf8')
+
+
+def response_error():
+    reply_error = 'HTTP/1.1 500 Internal Server Error\nContent-Type: text/plain\n\r\nSomething bad happened!'
+    return reply_error.encode('utf8')
 
 
 if __name__ == '__main__':
