d3bd93066a0fa0d687ff823aa4fb77fe5818d043
Rick Tesmond <tesmonrd@gmail.com>
2016-03-09 21:15:48 -0800

    STILL WORKING ON THIS

--- src/server.py
+++ src/server.py
@@ -16,29 +16,34 @@ def server():
 
     try:
         while True:
-            incoming_message = ''
-            response = response_ok()
-            buffer_length = 25
-            message_complete = False
-            while not message_complete:
-                part = conn.recv(buffer_length)
-                decoded = part.decode('utf8')
-                print(decoded)
-                incoming_message += decoded
-                parse_request(decoded)
-                if len(part) < buffer_length:
-                    break
-            print(incoming_message)
-            conn.sendall(response)
-            conn.close()
-            server.listen(1)
-            conn, addr = server.accept()
+            try:
+                incoming_message = ''
+                response = response_ok()
+                buffer_length = 25
+                message_complete = False
+                while not message_complete:
+                    part = conn.recv(buffer_length)
+                    decoded = part.decode('utf8')
+                    print(decoded)
+                    incoming_message += decoded
+                    parse_request(decoded)
+                    if len(part) < buffer_length:
+                        break
+                print(incoming_message)
+                try:
+                    conn.sendall(response(parse_request(incoming_message)))
+                except:
+                    pass
+                conn.close()
+                server.listen(1)
+                conn, addr = server.accept()
+            except:
+                response = response_error()
+                conn.sendall(response)
     except KeyboardInterrupt:
         print('KeyboardInterrupted')
         server.close()
-    except:
-        response = response_error()
-        conn.sendall(response)
+    finally:
         conn.close()
         server.listen(1)
         conn, addr = server.accept()
@@ -58,26 +63,22 @@ def parse_request(request):
     """A."""
     replaced_n = request.replace('\n', ' ')
     parsed_request = replaced_n.split('')
-    while True:
-        try:
-            parsed_request[0] == 'GET'
-        except IndexError:
-            print("Index Error, method must be 'GET'")
-            # continue
-
-        try:
-            parsed_request[2] == 'HTTP/1.1'
-        except IndexError:
-            print("Index Error, protocol must be 'HTTP/1.1'")
-            # continue
-        try:
-            parsed_request[3] == 'Host:'
-        except IndexError:
-            print("Index Error, header must be 'Host:'")
-            # continue
-        finally:
-            print(parsed_request[4])
-            return parsed_request[4]
+    try:
+        parsed_request[0] == 'GET'
+    except:
+        raise RuntimeError
+        print("405 error: Method must be 'GET'")
+    try:
+        parsed_request[2] == 'HTTP/1.1'
+    except:
+        raise RuntimeError
+        print("505 error: Protocol must be 'HTTP/1.1'")
+    try:
+        parsed_request[3:4] == 'Host:'
+    except:
+        raise RuntimeError
+        print("404 error")
+    return parsed_request
 
 
 if __name__ == '__main__':
