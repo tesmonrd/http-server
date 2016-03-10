c4aa08524722b89f26c29753caf6edbd4813fa60
Rick Tesmond <tesmonrd@gmail.com>
2016-03-09 18:52:52 -0800

    parser is set up working on function

--- src/server.py
+++ src/server.py
@@ -4,7 +4,7 @@ import socket
 
 
 def server():
-    """server responds with decoded bytes msg."""
+    """Server responds with decoded bytes msg."""
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     address = ('127.0.0.1', 5000)
@@ -15,20 +15,6 @@ def server():
     message_complete = False
 
     try:
-        while not message_complete:
-            part = conn.recv(buffer_length)
-            print(part.decode('utf8'))
-            conn.sendall(part)
-            if len(part) < buffer_length:
-                break
-    except KeyboardInterrupt:
-        print('KeyboardInterrupted')
-        conn.close()
-    except:
-        server.listen(1)
-        conn, addr = server.accept()
-
-    try:
         while True:
             incoming_message = ''
             response = response_ok()
@@ -37,7 +23,9 @@ def server():
             while not message_complete:
                 part = conn.recv(buffer_length)
                 decoded = part.decode('utf8')
+                print(decoded)
                 incoming_message += decoded
+                parse_request(decoded)
                 if len(part) < buffer_length:
                     break
             print(incoming_message)
@@ -64,6 +52,33 @@ def response_ok():
 def response_error():
     reply_error = 'HTTP/1.1 500 Internal Server Error\nContent-Type: text/plain\n\r\nSomething bad happened!'
     return reply_error.encode('utf8')
+
+
+def parse_request(request):
+    """A."""
+    replaced_n = request.replace('\n', ' ')
+    parsed_request = replaced_n.split('')
+    while True:
+        try:
+            parsed_request[0] == 'GET'
+        except IndexError:
+            print("Index Error, method must be 'GET'")
+            # continue
+
+        try:
+            parsed_request[2] == 'HTTP/1.1'
+        except IndexError:
+            print("Index Error, protocol must be 'HTTP/1.1'")
+            # continue
+        try:
+            parsed_request[3] == 'Host:'
+        except IndexError:
+            print("Index Error, header must be 'Host:'")
+            # continue
+        finally:
+            print(parsed_request[4])
+            return parsed_request[4]
+
 
 if __name__ == '__main__':
         server()
