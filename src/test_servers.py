5df8ca0aa38ab48e304b09e67d37f113e1d1ad74
Rick Tesmond <tesmonrd@gmail.com>
2016-03-13 21:31:58 -0700

    three working tests

--- src/test_servers.py
+++ src/test_servers.py
@@ -5,19 +5,19 @@ import pytest
 def test_method_error():
     """Test the parse_request GET error."""
     from server import parse_request
-    with pytest.raises(RuntimeError):
+    with pytest.raises(NameError):
         parse_request("PUT 127.0.0.1:5000 HTTP/1.1 \nHost:127.0.0.1\r\n")
 
 
 def test_version_error():
     """Test if the HHTP-version is correct."""
     from server import parse_request
-    with pytest.raises(RuntimeError):
+    with pytest.raises(TypeError):
         parse_request("GET 127.0.0.1:5000 HTTP/3.1 \nHost:127.0.0.1\r\n")
 
 
 def test_host_error():
     """Test if the host Header is correct."""
     from server import parse_request
-    with pytest.raises(RuntimeError):
+    with pytest.raises(ValueError):
         parse_request("GET 127.0.0.1:5000 HTTP/1.1 \nHos:127.0.0.1\r\n")
